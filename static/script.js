bddc14176e415d347a3514383040903b8514afd2
airtowF <airtow@protonmail.com>
2025-02-28 22:36:20 -0300

    corrigido bugs

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function toggleCandidateFields() {
-    var candidatoCheckbox = document.getElementById('candidato');
-    var candidateFields = document.getElementById('candidate-fields');
-    if (candidatoCheckbox.checked) {
-        candidateFields.classList.remove('hidden');
-    } else {
-        candidateFields.classList.add('hidden');
-    }
-}
